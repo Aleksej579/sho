d06106dd4246acee6c6ea15b6acd079ce935921f
Алексей Еременко <aleksej.eremenko579@gmail.com>
2017-03-23 18:48:25 +0200

    nav

--- js/main.js
+++ js/main.js
@@ -1,2 +1,16 @@
 'use strict';
 //scrol--------------------------------------------------
+$(window).scroll(function(){
+    // 200px от верха
+    if ($(window).scrollTop() > '950'){
+        $("nav")
+            .css("background-color", "rgba(0, 0, 0, 0.5)")
+            .css("border-bottom", "1px solid grey");
+        $("nav ul li a").css("color", "white");
+    }else {
+        $("nav")
+            .css("background-color", "rgba(0, 0, 0, 0.0)")
+            .css("border", "none");
+        $("nav ul li a").css("color", "#b3b3b3");
+    }
+});
